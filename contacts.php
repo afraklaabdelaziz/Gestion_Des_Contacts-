7c5e8acbf93fde1116a55ae32fc13cfbd4ef3a84
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-04-12 11:18:56 +0000

    cree page contacts pour gestion votre contacts

---
+++ contacts.php
@@ -0,0 +1,187 @@
+<?php
+$list1 = "<a href='profile.php'>Profile</a>";
+$list2 = "<a href='login.php'>Logout</a>";
+include 'components/head.php';
+include 'components/header.php';
+?>
+<div class="d-flex justify-content-between p-2 addContactFixed">
+    <p class="fw-bold">List contacts</p>
+    <div class="btn btn-info d-md-block addContact"><i class="fas fa-user-plus"></i><span class="d-none d-md-inline">Add new contact</span></div>
+</div>
+<!-- form to add contact -->
+<div class="modelAddC">
+    <form class="modal" method="POST">
+        <p class="h2">Add contact</p>
+        <i class="fas fa-times closeAddC btn position-absolute end-0 fs-3"></i>
+        <input class="rounded-pill p-2" type="text" placeholder="enter name contact" name="name">
+        <input class="rounded-pill p-2" type="email" placeholder="enter email contact" name="email">
+        <input class="rounded-pill p-2" type="text" placeholder="enter phone contact" name="phone">
+        <input class="rounded-pill p-2" type="text" placeholder="enter adresse contact" name="adresse">
+        <input class="rounded-pill p-2 bg-info" type="submit" value="Save" name="save">
+    </form>
+</div>
+<!-- form to update contact -->
+<div class="modelEditC">
+    <form class="modal" method="POST">
+        <p class="h2">Update profile</p>
+        <i class="fas fa-times closeEditC btn position-absolute end-0 fs-3"></i>
+        <input class="rounded-pill p-2" type="text" value="afrakla abdelaziz" name="name">
+        <input class="rounded-pill p-2" type="email" value="afraklabdelaziz@gmail.com" name="email">
+        <input class="rounded-pill p-2" type="text" value="0639616681" name="phone">
+        <input class="rounded-pill p-2" type="text" value="Mezguita, agdz, zagora" name="adresse">
+        <input class="rounded-pill p-2 bg-info" type="submit" value="Update" name="edit">
+    </form>
+</div>
+<div class="table-responsive d-md-flex justify-content-center">
+    <table class="table table-striped align-middle text-center">
+        <thead>
+            <tr>
+                <?php
+                $thead = ["img" => "", "name" => "Name", "email" => "Email", "phone" => "Phone", "adress" => "Adresse", "crud" => ""];
+                foreach ($thead as $key => $value) {
+
+                    echo "<th class='head'>$value</th>";
+                }
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td> <img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mezguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mezguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="rounded-circle" src="components/images/abdelaziz.jpg" height="60px" alt=""></td>
+                <td>afrakla abdelaziz</td>
+                <td>afraklaabdelaziz@gamil.com</td>
+                <td>0639616681</td>
+                <td> Mewguita, agdz, zagora</td>
+                <td>
+                    <i class="fal btn fa-edit text-info fw-bold fs-3 updateContact"></i>
+                    <i class="fas btn fa-trash-alt text-info fw-bold fs-3"></i>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<script src="javascript/contact.js"></script>
+</body>
+
+</html>
