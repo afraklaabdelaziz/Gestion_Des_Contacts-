660ac717adb0785547ebf554172bc719c54be4f2
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-04-12 11:20:16 +0000

    les images utiliser dans le projet et les page includer

---
+++ components/header.php
@@ -0,0 +1,15 @@
+<?php
+$list1;
+$list2 ;
+$list3;
+?>
+<header class="d-flex justify-content-between p-2 ContainerHeader position-sticky top-0">
+    <p class="mx-2 h4">Contact list</p>
+    <i class="far fa-bars btn d-inline d-md-none"></i>
+    <li class="d-none d-md-inline list">
+        <?php
+         echo $list1;
+         echo $list2;
+        ?>
+    </li>
+</header>
